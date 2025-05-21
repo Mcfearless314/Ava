e45af7ea1ec2cde1bcd631a224594e17a1273189
Miran <112928970+Mcfearless314@users.noreply.github.com>
2025-05-21 13:45:13 +0200

    Final clean up

--- Ava.API/Controllers/UserController.cs
+++ Ava.API/Controllers/UserController.cs
@@ -47,7 +47,6 @@ public class UserController : ControllerBase
     return Ok(userDtos);
   }
 
-  [Authorize(Policy = "MustBeAdmin")]
   [HttpPost("updateUser")]
   public async Task<IActionResult> Update([FromBody] CredentialsDto dto)
   {
